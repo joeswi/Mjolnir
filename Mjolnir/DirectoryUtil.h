f78dbd8cf55529b35c11bb7a8c7d49a656919c9b
joeswi <joeswi@gmail.com>
2014-12-04 21:37:56 +0800

    目录工具类

---
+++ Mjolnir/DirectoryUtil.h
@@ -0,0 +1,13 @@
+//
+//  DirectoryUtil.h
+//  Mjolnir
+//
+//  Created by 夏 小为 on 14/12/4.
+//  Copyright (c) 2014年 XXW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DirectoryUtil : NSObject
+
+@end
---
+++ Mjolnir/DirectoryUtil.m
@@ -0,0 +1,13 @@
+//
+//  DirectoryUtil.m
+//  Mjolnir
+//
+//  Created by 夏 小为 on 14/12/4.
+//  Copyright (c) 2014年 XXW. All rights reserved.
+//
+
+#import "DirectoryUtil.h"
+
+@implementation DirectoryUtil
+
+@end
