5d5dfb1e14dac82168a58750f08c41e4b93ee309
joeswi <joeswi@gmail.com>
2015-06-10 19:25:35 +0800

    修改xcode6.3不能使用

--- Mjolnir/Mjolnir.m
+++ Mjolnir/Mjolnir.m
@@ -59,59 +59,56 @@ static Mjolnir *sharedPlugin;
         self.bundle = plugin;
         
         // Create menu items, initialize UI, etc.
-        NSMenuItem *productMenu = [[NSApp mainMenu] itemWithTitle:@"Product"];
-        if (productMenu)
-        {
-            [[productMenu submenu] addItem:[NSMenuItem separatorItem]];
-            
-            // Main Menu: Mjolnir
-            NSMenuItem *mjolnirMenu = [[productMenu submenu] addItemWithTitle:@"Mjolnir"
-                                                                       action:nil
-                                                                keyEquivalent:@""];
-            mjolnirMenu.submenu = [[NSMenu alloc] initWithTitle:@"Mjolnir"];
-            
-            // Mjolnir -> CIBuild
-            NSMenuItem *cibuild = [[mjolnirMenu submenu] addItemWithTitle:@"CIBuild"
-                                                                   action:@selector(ciBuildMenuOnClick)
-                                                            keyEquivalent:@""];
-            cibuild.target = self;
-            
-            // Mjolnir -> Analyze
-            NSMenuItem *analyze = [[mjolnirMenu submenu] addItemWithTitle:@"Consistency Analyze"
-                                                                   action:@selector(analyzeMenuOnClick)
-                                                            keyEquivalent:@""];
-            analyze.target = self;
-   
-            // Mjolnir -> Http Mock
-            NSMenuItem *httpMock = [[mjolnirMenu submenu] addItemWithTitle:@"HTTP Service Mock"
-                                                                    action:@selector(httpMockMenuOnClick)
-                                                             keyEquivalent:@""];
-            httpMock.target = self;
-            
-            // Mjolnir -> Goto
-            NSMenuItem *gotoMenu = [[mjolnirMenu submenu] addItemWithTitle:@"Goto" action:nil keyEquivalent:@""];
-            gotoMenu.submenu = [[NSMenu alloc] initWithTitle:@"Goto"];
-            
-            
-            // Mjolnir -> Goto -> Continuous Integration
-            NSMenuItem *gotoCI = [[gotoMenu submenu] addItemWithTitle:@"Continuous Integration"
-                                                                action:@selector(gotoCIMenuOnClick)
-                                                         keyEquivalent:@""];
-            gotoCI.target = self;
-            
-            // Mjolnir -> Goto -> Crash Explorer
-            NSMenuItem *gotoCrashExplorer = [[gotoMenu submenu] addItemWithTitle:@"Crash Explorer"
-                                                               action:@selector(gotoCrashExplorerMenuOnClick)
+        NSMenu *mainMenu = [NSApp mainMenu];
+        
+        // Main Menu: Mjolnir
+        NSMenuItem *mjolnirMenu = [mainMenu addItemWithTitle:@"Mjolnir"
+                                                      action:nil
+                                               keyEquivalent:@""];
+        mjolnirMenu.submenu = [[NSMenu alloc] initWithTitle:@"Mjolnir"];
+        
+        // Mjolnir -> CIBuild
+        NSMenuItem *cibuild = [[mjolnirMenu submenu] addItemWithTitle:@"CIBuild"
+                                                               action:@selector(ciBuildMenuOnClick)
                                                         keyEquivalent:@""];
-            gotoCrashExplorer.target = self;
-            
-            // Mjolnir -> Goto -> Mjolnir Console
-            NSMenuItem *gotoMjolnirConsole = [[gotoMenu submenu] addItemWithTitle:@"Mjolnir Console"
-                                                                           action:@selector(gotoMjolnirConsoleMenuOnClick)
-                                                                    keyEquivalent:@""];
-            gotoMjolnirConsole.target = self;
-            
-        }
+        cibuild.target = self;
+        
+        // Mjolnir -> Analyze
+        NSMenuItem *analyze = [[mjolnirMenu submenu] addItemWithTitle:@"Consistency Analyze"
+                                                               action:@selector(analyzeMenuOnClick)
+                                                        keyEquivalent:@""];
+        analyze.target = self;
+
+        // Mjolnir -> Http Mock
+        NSMenuItem *httpMock = [[mjolnirMenu submenu] addItemWithTitle:@"HTTP Service Mock"
+                                                                action:@selector(httpMockMenuOnClick)
+                                                         keyEquivalent:@""];
+        httpMock.target = self;
+        
+        // Mjolnir -> Goto
+        NSMenuItem *gotoMenu = [[mjolnirMenu submenu] addItemWithTitle:@"Goto" action:nil keyEquivalent:@""];
+        gotoMenu.submenu = [[NSMenu alloc] initWithTitle:@"Goto"];
+        
+        
+        // Mjolnir -> Goto -> Continuous Integration
+        NSMenuItem *gotoCI = [[gotoMenu submenu] addItemWithTitle:@"Continuous Integration"
+                                                            action:@selector(gotoCIMenuOnClick)
+                                                     keyEquivalent:@""];
+        gotoCI.target = self;
+        
+        // Mjolnir -> Goto -> Crash Explorer
+        NSMenuItem *gotoCrashExplorer = [[gotoMenu submenu] addItemWithTitle:@"Crash Explorer"
+                                                           action:@selector(gotoCrashExplorerMenuOnClick)
+                                                    keyEquivalent:@""];
+        gotoCrashExplorer.target = self;
+        
+        // Mjolnir -> Goto -> Mjolnir Console
+        NSMenuItem *gotoMjolnirConsole = [[gotoMenu submenu] addItemWithTitle:@"Mjolnir Console"
+                                                                       action:@selector(gotoMjolnirConsoleMenuOnClick)
+                                                                keyEquivalent:@""];
+        gotoMjolnirConsole.target = self;
+        
+   
     }
     return self;
 }
